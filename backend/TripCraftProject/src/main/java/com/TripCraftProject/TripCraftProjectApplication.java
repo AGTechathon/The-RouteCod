33b4410263d73a10d1b4438fda7c484184f1dcf2
AnkitaAdvitot <110318342+AnkitaAdvitot@users.noreply.github.com>
2025-06-14 14:05:23 +0530

    SpringBoot Project Structure Added

---
+++ backend/TripCraftProject/src/main/java/com/TripCraftProject/TripCraftProjectApplication.java
@@ -0,0 +1,13 @@
+package com.TripCraftProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripCraftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripCraftProjectApplication.class, args);
+	}
+
+}
---
+++ backend/TripCraftProject/src/test/java/com/TripCraftProject/TripCraftProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TripCraftProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCraftProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
