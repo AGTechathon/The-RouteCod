e4ffbe5420fae8bb62c41beaea48d230ab5a9c2a
Priyanka Sundalam <ksundalam@gmail.com>
2025-06-14 17:10:59 +0530

    Integrated Login Signup with backend

---
+++ frontend/src/services/api.js
@@ -0,0 +1,23 @@
+// src/services/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL, // Proxied to backend
+  withCredentials: true, // Include HTTP-only cookies in requests
+});
+
+// Optional: Handle 401 errors (token expiration)
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       // Only redirect if not already on the login page
+//       if (window.location.pathname !== '/login' && window.location.pathname !== '/signup') {
+//         window.location.href = '/login';
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default api;
---
+++ frontend/src/services/authService.js
@@ -0,0 +1,26 @@
+// src/services/authService.js
+import toast from 'react-hot-toast';
+import api from './api';
+
+export const register = async (userData) => {
+    const response = await api.post('/auth/register', userData);
+    return response;
+};
+
+
+export const login = async (email, password) => {
+    const response = await api.post('/auth/login', { email, password });
+    
+    return response; // "login successful"
+};
+
+export const logout = async () => {
+    try {
+        const response = await api.post('/auth/logout');
+        window.location.href = '/login'; // or use navigate('/login') if using react-router
+        toast.success(response.data.message);
+      } catch (err) {
+        toast.success("Logout failed!");
+        console.error('Logout failed', err);
+      }
+};
