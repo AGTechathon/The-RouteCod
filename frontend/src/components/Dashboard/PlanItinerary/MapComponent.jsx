384d348569916153d5b8c02205c407c4b34c3698
Priyanka Sundalam <ksundalam@gmail.com>
2025-06-15 04:50:47 +0530

    Updated folder structure

--- frontend/src/components/Dashboard/PlanItinerary/MapComponent.jsx
+++ frontend/src/components/Dashboard/PlanItinerary/MapComponent.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "../../../../public/css/MapComponent.css";
+import "/public/css/MapComponent.css";
 
 // Remove default icon config
 delete L.Icon.Default.prototype._getIconUrl;
--- frontend/src/components/Dashboard/Sidebar/Logo.jsx
+++ frontend/src/components/Dashboard/Sidebar/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <div className="p-6 flex items-center gap-2">
       <div className="bg-emerald-600 p-2 rounded-lg">
-       <img src="./../public/img/tree-palm-white.png" alt="" className='w-6 h-6'/>
+       <img src="/public/img/tree-palm-white.png" alt="" className='w-6 h-6'/>
       </div>
       <span className="text-2xl font-bold bg-gradient-to-r from-emerald-600 to-emerald-400 bg-clip-text text-transparent">
         TripCraft
