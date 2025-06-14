f340bbb744c635a54ebdc0cdac2a5c27a51fe49e
Priyanka Sundalam <ksundalam@gmail.com>
2025-06-14 17:12:22 +0530

    Updated Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,38 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import { Toaster } from "react-hot-toast";
-import ServiceSection from "./components/ServiceSection";
-import Reviews from "./components/Reviews";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ServiceSection from './components/ServiceSection';
+import Reviews from './components/Reviews';
+import Footer from './components/Footer';
+import { Toaster } from 'react-hot-toast';
+import ProtectedRoute from './routes/ProtectedRoute';
+import DashboardRoutes from './routes/DashboardRoutes';
+import LoginRedirectWrapper from './routes/LoginRedirectWrapper';
+import SignupRedirectWrapper from './routes/SignupRedirectWrapper';
+
 function App() {
   return (
     <Router>
       <div className="relative">
         <Toaster position="top-right" />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar />
-                <Home />
-                <ServiceSection />
-                <Reviews />
-              </>
-            }
-          />
+          <Route path="/" element={
+            <>
+              <Navbar />
+              <Home />
+              <ServiceSection />
+              <Reviews />
+              <Footer />
+            </>
+          } />
+          <Route path="/login" element={<LoginRedirectWrapper />} />
+          <Route path="/signup" element={<SignupRedirectWrapper />} />
+          <Route path="/dashboard/*" element={
+            <ProtectedRoute>
+              <DashboardRoutes />
+            </ProtectedRoute>
+          } />
         </Routes>
       </div>
     </Router>
